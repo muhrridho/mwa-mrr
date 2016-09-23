d1bd5d63e7c9a18bee1cc71727cbca05637709c5
Muhammad Rasyid Ridho <muhammad.ridho@gdn-commerce.com>
2016-09-23 14:28:27 +0700

    no message

--- blocks/mobile/panel/_panel.js
+++ blocks/mobile/panel/_panel.js
@@ -34,67 +34,67 @@ function initPanel(){
 	// $(".page__item").append('<div class="panel__overlay"></div>');
 	// $("body").append('<div class="panel__overlay"></div>');
 
-	$(".js-panel__handler, .panel, .panel__overlay")
-	.on("touchstart", function(ev) {
-		var e = ev.originalEvent;
-		isDraggable = true;
-		isTouchOnly = true;
+	// $(".js-panel__handler, .panel, .panel__overlay")
+	// .on("touchstart", function(ev) {
+	// 	var e = ev.originalEvent;
+	// 	isDraggable = true;
+	// 	isTouchOnly = true;
 
-		distance = 0;
-		element = $(this);
+	// 	distance = 0;
+	// 	element = $(this);
 
-		// Check whick element are touched & and calculate based on it
-		mouseX = $(this).hasClass("panel") ? panelWidth - e.touches[0].pageX : 
-			$(this).hasClass("panel__overlay") ? panelWidth - e.touches[0].pageX : 0;
+	// 	// Check whick element are touched & and calculate based on it
+	// 	mouseX = $(this).hasClass("panel") ? panelWidth - e.touches[0].pageX : 
+	// 		$(this).hasClass("panel__overlay") ? panelWidth - e.touches[0].pageX : 0;
 
-		// Disable css transitions
-		$('.page').addClass("page--notransition");
-	})
-	.on("touchmove", function(ev) {
-		var e = ev.originalEvent;
-		if(isDraggable === false) return;
-		distance = mouseX + e.touches[0].pageX;
-		isTouchOnly = false;
+	// 	// Disable css transitions
+	// 	$('.page').addClass("page--notransition");
+	// })
+	// .on("touchmove", function(ev) {
+	// 	var e = ev.originalEvent;
+	// 	if(isDraggable === false) return;
+	// 	distance = mouseX + e.touches[0].pageX;
+	// 	isTouchOnly = false;
 
-		$(".test-fixed").html(distance);
+	// 	$(".test-fixed").html(distance);
 
-		// Set maximum & minimum page would slide
-		if(distance >= 0 && distance <= panelWidth){
-			// $(".page").css('transform', 'translate3d('+distance+'px, 0, 0)');
-			// $(".panel").css('left', (-1 * panelWidth) + distance);
-			$(".panel").css('margin-left', distance);
-			$(".panel").addClass('panel--notransitions');
+	// 	// Set maximum & minimum page would slide
+	// 	if(distance >= 0 && distance <= panelWidth){
+	// 		// $(".page").css('transform', 'translate3d('+distance+'px, 0, 0)');
+	// 		// $(".panel").css('left', (-1 * panelWidth) + distance);
+	// 		$(".panel").css('margin-left', distance);
+	// 		$(".panel").addClass('panel--notransitions');
 
-			// Set overlay opacity
-			$(".app__overlay").addClass("app__overlay--show");
-			$(".app__overlay").fadeTo(0, distance/panelWidth);
-		}
+	// 		// Set overlay opacity
+	// 		$(".app__overlay").addClass("app__overlay--show");
+	// 		$(".app__overlay").fadeTo(0, distance/panelWidth);
+	// 	}
 
-		$("#status").html("distance:" + distance);
-	})
-	$(document).on("touchend", function(ev) {
-		var e = ev.originalEvent;
+	// 	$("#status").html("distance:" + distance);
+	// })
+	// $(document).on("touchend", function(ev) {
+	// 	var e = ev.originalEvent;
 		
-		if (!isDraggable) return;
-		isDraggable = false;
+	// 	if (!isDraggable) return;
+	// 	isDraggable = false;
 
-		// Enable css transitions
-		$(".panel").removeClass('panel--notransitions');
+	// 	// Enable css transitions
+	// 	$(".panel").removeClass('panel--notransitions');
 
-		// if($('.panel').hasClass("panel--show")){
-		// 	// Set the distance back to "normal" if element touched is not js-panel__handler
-		// 	if(!element.hasClass('js-panel__handler')) distance = panelWidth - distance;
+	// 	// if($('.panel').hasClass("panel--show")){
+	// 	// 	// Set the distance back to "normal" if element touched is not js-panel__handler
+	// 	// 	if(!element.hasClass('js-panel__handler')) distance = panelWidth - distance;
 
-		// 	// Fixes problem when only touch (not dragging) panel will closes the panel.
-		// 	if(!element.hasClass("panel__overlay") && isTouchOnly) return;
+	// 	// 	// Fixes problem when only touch (not dragging) panel will closes the panel.
+	// 	// 	if(!element.hasClass("panel__overlay") && isTouchOnly) return;
 
-		// 	distance > panelWidth/6 ? hidePanel() : showPanel();
-		// } else {
-		// 	distance < panelWidth/6 ? hidePanel() : showPanel();
-		// }
+	// 	// 	distance > panelWidth/6 ? hidePanel() : showPanel();
+	// 	// } else {
+	// 	// 	distance < panelWidth/6 ? hidePanel() : showPanel();
+	// 	// }
 
-		$("#status").html("mouse up");
-	})
+	// 	$("#status").html("mouse up");
+	// })
 
 	$(".panel__overlay").click(function(){
 		hidePanel();
--- pages/mobile/_fundamental.js
+++ pages/mobile/_fundamental.js
@@ -1,7 +1,3 @@
 $(document).ready(function(){
 	initPanel();
-	$("p").click(function(){
-		$(".panel").toggleClass("panel--show");
-		$(".app__overlay").toggleClass("app__overlay--show");
-	})
 })
