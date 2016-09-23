6dee45bfd019f4d5684c928d1a7d03314d23b49f
Muhammad Rasyid Ridho <muhammad.ridho@gdn-commerce.com>
2016-09-23 13:40:47 +0700

    0.1.1

--- pages/mobile/_fundamental.js
+++ pages/mobile/_fundamental.js
@@ -2,5 +2,6 @@ $(document).ready(function(){
 	// initPanel();
 	$('body').click(function(){
 		$(".panel").toggleClass("panel--show");
+		$(".app__overlay").toggleClass("app__overlay--show");
 	})
 })
