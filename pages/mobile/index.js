82df8ee3f709ddfcca56d0969f9f30340ff35385
Muhammad Rasyid Ridho <muhammad.ridho@gdn-commerce.com>
2016-09-27 15:10:08 +0700

    dev

--- pages/mobile/index.js
+++ pages/mobile/index.js
@@ -11,4 +11,24 @@ $(document).ready(function(){
 		showPage("#page-home");
 		return false;
 	})
+	$('.slider').owlCarousel({
+		autoPlay: true,
+		autoplayTimeout: 5000,
+		singleItem : true,
+		transitionStyle : "fade",
+
+	});
+
+	// //  Let's say we want to use $.getScript to load our scripts for some reason
+	// var scripts = [
+	// 	'http://stephband.info/jquery.event.move/js/jquery.event.move.js',
+	// 	'http://stephband.info/jquery.event.swipe/js/jquery.event.swipe.js'
+	// ];
+
+	// $.getScript(scripts[0]);
+
+	// //  Once our script is loaded, we can initSwipe to add swipe support
+	// $.getScript(scripts[1], function() {
+	// 	$('.slider').unslider('initSwipe');
+	// });
 })
