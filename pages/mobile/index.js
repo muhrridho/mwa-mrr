dae499606e2a72ca5ca2273fbd23ca5b04ba8801
Muhammad Rasyid Ridho <muhammad.ridho@gdn-commerce.com>
2016-09-27 11:21:04 +0700

    dev

--- pages/mobile/index.js
+++ pages/mobile/index.js
@@ -4,4 +4,11 @@ $(document).ready(function(){
 	$(".js-header__nav--hamburger").click(function(){
 		toggleSidebar();
 	})
+	$(".js-search__ac-trigger").click(function(){
+		showPage("#page-suggestion");
+	})
+	$(".js-search__close").click(function(){
+		showPage("#page-home");
+		return false;
+	})
 })
