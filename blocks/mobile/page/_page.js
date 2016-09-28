4cad1a8a7ea777a878653e3b78a7430454945f88
Muhammad Rasyid Ridho <muhammad.ridho@gdn-commerce.com>
2016-09-28 14:10:28 +0700

    dev

--- blocks/mobile/page/_page.js
+++ blocks/mobile/page/_page.js
@@ -1,7 +1,7 @@
 function showPage(id){
 	$(".page__item").not($(id)).addClass("page__item--hidden");
 	$(id).removeClass("page__item--hidden");
-	
+
 	// Fix an issue on iPhone when orientation is landscape. Header is not displayed properly
 	$(window).scrollTop(0);
 }
@@ -9,6 +9,10 @@ function showPage(id){
 function initPage(){
 	$(".js-page--show").click(function(){
 		var page = $(this).attr("href");
+
+		// For testing on real device
+		page = page.replace(window.location.href, "")
+
 		showPage(page);
 		return false;
 	})
