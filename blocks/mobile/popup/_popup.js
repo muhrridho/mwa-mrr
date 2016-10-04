a28b7c0613d48152ffad100129b90b04799004f4
Muhammad Rasyid Ridho <muhammad.ridho@gdn-commerce.com>
2016-10-04 14:28:41 +0700

    dev

--- blocks/mobile/popup/_popup.js
+++ blocks/mobile/popup/_popup.js
@@ -1,13 +1,32 @@
 function showPopup(id){
-	$(id).css({
-		top: ($(window).height() / 2) - $(id).outerHeight()/2,
-		left: ($(window).width() / 2) - $(id).outerWidth()/2
+	showOverlay();
+	$(".popup--show").removeClass("popup--is-show");
+	$(".app__overlay").on("click.popup", function(){
+		hidePopup();;
 	})
+	$(id)
+		.css({
+			top: ($(window).height() / 2) - $(id).outerHeight()/2,
+			left: ($(window).width() / 2) - $(id).outerWidth()/2
+		})
+		.addClass("popup--is-show");
+}
+function hidePopup() {
+	hideOverlay();
+	$(".app__overlay").off("click.popup");
+	$(".popup").removeClass("popup--is-show");
 }
 
 function initPopup() {
-	$(".js-popup--show").click(function(){
+	$(".js-popup--show").click(function(e){
 		var id = $(this).attr("test-href");
-		showPage(id);
+		showPopup(id);
+		e.preventDefault();
+		return false;
+	})
+	$(".js-popup--hide").click(function(e){
+		hidePopup();
+		e.preventDefault();
+		return false;
 	})
 }
--- pages/mobile/_fundamental.js
+++ pages/mobile/_fundamental.js
@@ -1,3 +1,11 @@
+function showOverlay(){
+	$(".app__overlay").addClass("app__overlay--show");
+	$("html, body").css("overflow", "hidden");
+}
+function hideOverlay(){
+	$(".app__overlay").removeClass("app__overlay--show");
+	$("html, body").css("overflow", "auto");
+}
 $(document).ready(function(){
 	initPage();
 })
