d5bde614c41366801e177e08b84b5924acc7aade
Muhammad Rasyid Ridho <muhammad.ridho@gdn-commerce.com>
2016-10-04 11:15:16 +0700

    dev

--- blocks/mobile/room-qty/_room-qty.js
+++ blocks/mobile/room-qty/_room-qty.js
@@ -9,10 +9,10 @@ function initRoomQty() {
 function showRoomQty(){
 	$(".room-qty__overlay").addClass("room-qty__overlay--is-show");
 	$(".room-qty__select").addClass("room-qty__select--is-show");
-	$("body").css("overflow", "hidden");
+	$(".app").css("overflow-y", "hidden");
 }
 function hideRoomQty(){
 	$(".room-qty__overlay").removeClass("room-qty__overlay--is-show");
 	$(".room-qty__select").removeClass("room-qty__select--is-show");
-	$("body").css("overflow", "auto");
+	$(".app").css("overflow-y", "auto");
 }
